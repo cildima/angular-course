d8ac7ff2d7c35e27a60f79e4b34d82b0adaf9523
Dmitriy Silitskas <dmitriy.silitckas@mediapark.lt>
2020-03-17 22:56:52 +0200

    Basic Service

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -13,12 +13,13 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
   ],
   imports: [
     BrowserModule,
@@ -30,7 +31,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule,
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
